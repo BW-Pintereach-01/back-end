06a93ef77c8fbf1ebfb28039f60b916c308ee54a
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-05-26 12:38:25 -0400

    trying to get the seeds to work

--- data/migrations/20200525145933_create-pintereach-table.js
+++ data/migrations/20200525145933_create-pintereach-table.js
@@ -1,15 +1,48 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('users', users => {
-    users.increments();
-
-    users
-      .string('username', 255)
-      .notNullable()
-      .unique();
-    users.string('password', 255).notNullable();
-  });
-};
-
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
-};
+    return knex.schema.createTable('users', users => {
+      users.increments();
+  
+      users
+        .string('username', 255)
+        .notNullable()
+        .unique();
+      users.string('password', 255).notNullable();
+    })
+  
+  .createTable('categories', tbl => {
+      tbl.increments();
+  
+      tbl.string('category').notNullable();
+  })
+  .createTable('articles', tbl => {
+    tbl.increments();
+  
+  
+    tbl.integer('users_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete("RESTRICT");
+  
+    tbl.string('link', 255).notNullable();
+    tbl.string('title', 225);
+    tbl.string('author', 225).notNullable();
+  
+    tbl.integer('category_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('categories')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT')
+  })
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users')
+    .dropTableIfExists('categories')
+    .dropTableIfExists('articles');
+  };
+  
--- data/seeds/02_categories.js
+++ data/seeds/02_categories.js
@@ -4,9 +4,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('catergories').insert([
-        {id: 1, category: 'horror', users_id:'1'},
-        {id: 2, category: 'comedy', users_id:'1'},
-        {id: 3, category: 'sports', users_id:'1'}
+        {id: 1, category: 'horror', users_id:1},
+        {id: 2, category: 'comedy', users_id:1},
+        {id: 3, category: 'sports', users_id:1}
       ]);
     });
 };
