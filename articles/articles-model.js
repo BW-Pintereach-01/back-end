f238a6929e2ab310996596668d7d502a7c070516
Devin_Moore <d.j.moore209@gmail.com>
2020-05-27 15:18:04 -0400

    added user crud ops to user router

--- articles/articles-model.js
+++ articles/articles-model.js
@@ -1,49 +1,51 @@
 const db = require('./../data/dbConfig');
 
 module.exports = {
-    insert,
-    remove,
-    update,
-    findAllArticles,
-    findUserArticles,
-    findArticleById
+  insert,
+  remove,
+  update,
+  findAllArticles,
+  findUserArticles,
+  findArticleById
 }
 
 function insert(article) {
-    return db('articles')
-    .insert(article, 'id')
-    .then(ids => ({id: ids[0]}));
+  return db('articles')
+  .insert(article, 'id')
+  .then(ids => ({id: ids[0]}));
 }
 
 function remove(id) {
-    return db('articles')
-        .where('id', Number(id))
-        .del();
+  return db('articles')
+      .where('id', Number(id))
+      .del();
 }
 
 function update(changes, id) {
-    return db('articles')
-      .where({ id })
-      .update(changes)
-      .then(success => {
-        if(success) {
-          return findArticleById(id)
-        } else {
-          return res.status(500).json({ message: 'Failed to update article' });
-        }
-      })
-  }
+  return db('articles')
+    .where({ id })
+    .update(changes)
+    .then(success => {
+      if(success) {
+        return findArticleById(id)
+      } else {
+        return res.status(500).json({ message: 'Failed to update article' });
+      }
+    })
+}
 
-  function findAllArticles() {
-    return db('articles')
-  }
-  function findArticleById(id) {
-    return db('articles').where({ id }).first();
-  }
-  function findUserArticles(id) {
-    return db('users as u')
-      .join('articles as a', 'a.users_id', '=', 'u.id')
-      .select('a.link', 'a.title', 'a.author', 'a.category')
-      .distinct('a.title')
-      .where('a.users_id', '=', id)
-  }
+function findAllArticles() {
+  return db('articles')
+}
+
+function findArticleById(id) {
+  return db('articles').where({ id }).first();
+}
+
+function findUserArticles(id) {
+  return db('users as u')
+    .join('articles as a', 'a.users_id', '=', 'u.id')
+    .select('a.link', 'a.title', 'a.author', 'a.category')
+    .distinct('a.title')
+    .where('a.users_id', '=', id)
+}
--- articles/articles-router.js
+++ articles/articles-router.js
@@ -3,106 +3,110 @@ const Articles = require('./articles-model.js');
 // const restrict = require('../auth/auth-middleware.js');
 
 router.post('/',validateArticle,(req,res) => {
-    const {users_id, title, author, link,} = req.body;
-    Articles.insert({users_id, title, author, link,})
-        .then(article => res.status(200).json(article))
-        .catch(err => {
-            console.log(err)
-        }) 
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error:'Sorry error adding article'})
-        })
-})
-
-function validateArticle(req, res, next) {
-    const article = req.body;
-    if (Object.keys(article).length > 0) {
-      if(article.link && article.author) {
-        next();
-      } else {
-        res.status(400).json({
-          message: 'Missing required field, please enter link and author'
-        })
-      }
-    } else {
-      res.status(400).json({
-        message: 'Missing post data'
+  const {users_id, title, author, link,} = req.body;
+  Articles.insert({users_id, title, author, link,})
+      .then(article => res.status(200).json(article))
+      .catch(err => {
+          console.log(err)
+      }) 
+      .catch(err => {
+          console.log(err);
+          res.status(500).json({error:'Sorry error adding article'})
       })
-    }
-  }
+})
 
 router.delete('/:id', (req,res) => {
-    const { id } = req.params;
-    Articles.remove(id)
-    .then(deleted => {
-        if (deleted) {
-            res.status(204).end();
-        } else {
-            res.status(404).json({error: "Article with ID does not exist"});
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: "Server error deleting"})
-    })
+  const { id } = req.params;
+  Articles.remove(id)
+  .then(deleted => {
+    if (deleted) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({error: "Article with ID does not exist"});
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({error: "Server error deleting"})
+  })
 })
-
+    
 router.get('/', (req, res) => {
-    Articles.findAllArticles()
-    .then(articles => {
-      res.status(200).json({articles});
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to get articles' });
-    });
+  Articles.findAllArticles()
+  .then(articles => {
+    res.status(200).json({articles});
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get articles' });
   });
-  router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    Articles.findArticleById(id)
-    .then(article => {
-      if (article) {
-        res.status(200).json({data: article});
-      } else {
-        res.status(404).json({ message: 'Could not find article with given id.' })
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to get article', err });
-    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Articles.findArticleById(id)
+  .then(article => {
+    if (article) {
+      res.status(200).json({data: article});
+    } else {
+      res.status(404).json({ message: 'Could not find article with given id.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get article', err });
   });
-  router.get('/user/:id', (req, res) => {
-    const { id } = req.params;
-    Articles.findUserArticles(id)
-    .then(([articles]) => {
-      if (articles) {
-        const articleArr = []
-        res.status(200).json({ articles: [...articleArr, articles] });
-      } else {
-        res.status(404).json({ message: 'User has no articles in the Database' })
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to get article', err });
-    });
+});
+
+router.get('/user/:id', (req, res) => {
+  const { id } = req.params;
+  Articles.findUserArticles(id)
+  .then(([articles]) => {
+    if (articles) {
+      const articleArr = []
+      res.status(200).json({ articles: [...articleArr, articles] });
+    } else {
+      res.status(404).json({ message: 'User has no articles in the Database' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get article', err });
   });
-  router.put('/:id', validateArticle, (req, res) => {
-    const { id } = req.params;
-    const changes = req.body;
-    Articles.findArticleById(id)
-    .then(article => {
-      if (article) {
-        Articles.update(changes, id)
-        .then(updatedArticle => {
-          res.json(updatedArticle);
-        });
-      } else {
-        res.status(404).json({ message: 'Could not find article with given id' });
-      }
-    })
-    .catch (err => {
-      res.status(500).json({ message: 'Failed to update article', err });
-    });
+});
+
+router.put('/:id', validateArticle, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  Articles.findArticleById(id)
+  .then(article => {
+    if (article) {
+      Articles.update(changes, id)
+      .then(updatedArticle => {
+        res.json(updatedArticle);
+      });
+    } else {
+      res.status(404).json({ message: 'Could not find article with given id' });
+    }
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to update article', err });
   });
+});
+    
+function validateArticle(req, res, next) {
+  const article = req.body;
+  if (Object.keys(article).length > 0) {
+    if(article.link && article.author) {
+      next();
+    } else {
+      res.status(400).json({
+        message: 'Missing required field, please enter link and author'
+      })
+    }
+  } else {
+    res.status(400).json({
+      message: 'Missing post data'
+    })
+  }
+}
 
 module.exports = router;
+    
