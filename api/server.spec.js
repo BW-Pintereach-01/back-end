f6d3b250df2e42a1918c373df62700c53f1cfa94
Patrick Dal Gobbo <juandalgobbo@Juans-MacBook-Air.local>
2020-05-27 16:54:48 -0400

    working on tests

---
+++ api/server.spec.js
@@ -0,0 +1,26 @@
+
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('server', function() {
+    it('should run tests', function() {
+        expect(true).toBe(true)
+    })
+
+    describe('GET /', function() {
+        it('should return 200 ok', function() {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.status).toBe(200)
+                })
+        })
+
+        // it('should return JSON', function() {
+        //     return request(server).get('/')
+        //         .then(res => {
+        //             expect(res.type).toMatch(/json/i)
+        //         })
+        // })
+    })
+})
