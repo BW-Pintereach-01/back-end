10fe9ec22d2e73a65c1f9779ccc1c7bd4078f97e
Devin_Moore <d.j.moore209@gmail.com>
2020-05-28 14:31:58 -0400

    added test to user router

--- users/users.test.js
+++ users/users.test.js
@@ -5,6 +5,7 @@ const db = require('../data/dbConfig')
 beforeEach(async () => {
   return db.migrate.rollback()
     .then(() => db.migrate.latest())
+    .then(() => db.seed.run())
 });
 
 test('POST /api/users/register to be successful', async () => {
@@ -31,3 +32,36 @@ test('POST /api/users/login to be successful', async () => {
   // console.log(res.body)
 })
 
+test('Get all users', async () => {
+  const res = await request(server)
+    .get('/api/users')
+  expect(res.status).toBe(200)
+  expect(res.body.users).toHaveLength(1)
+})
+
+test('Get user id to get individual user', async () => {
+  const res = await request(server)
+    .get('/api/users/1')
+  expect(res.status).toBe(200)
+  expect(res.body.data).toHaveProperty('id', 1)
+})
+
+test('Edit username on put request', async () => {
+  const res = await request(server)
+    .put('/api/users/1')
+    .send({username: 'april'})
+  expect(res.status).toBe(200)
+  expect(res.body).toHaveProperty('username', 'april')
+})
+
+test('Delete user to not exist in db after delete', async () => {
+  const res = await request(server)
+    .delete('/api/users/1')
+  expect(res.status).toBe(204)
+  const resUser = await request(server)
+    .get('/api/users/1')
+    .then(response => {
+      expect(response.status).toBe(404)
+      expect(response.body).toHaveProperty('message', 'Could not find user with given id.')
+    })
+})
